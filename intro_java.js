5fe274910696e13d060e9af368d01732b9d735ac
absar uddin <absaruddin849@gmail.com>
2023-01-25 20:56:24 +0000

    first commit

---
+++ intro_java.js
@@ -0,0 +1,222 @@
+// var a=10;
+// var b=5;
+// var result=a+b;
+// console.log(result);
+// console.log(typeof b);
+// var F_Name="Mohammad";
+// var Middle_Name="Absar";
+// var L_Name="Uddin";
+// console.log(typeof F_Name);
+// console.log(typeof Middle_Name);
+// console.log(F_Name+Middle_Name+L_Name);
+// var D_B="08/12/1991";
+// console.log(D_B);
+// console.log(typeof D_B);
+// var _Personal_details={F_Name:"Mohammad",L_Name:"Uddin" ,DoB:"08/12/1991"};
+// console.log(_Personal_details);
+// console.log(typeof _Personal_details);
+
+// var FName="Absar";
+// LName="Uddin";
+// console.log(FName+LName);
+// var FName="Mohammad Absar";
+// LName="uddin";
+// function Name (FName,LName){
+//     result=FName+LName;
+//     console.log(result);
+
+// }
+// Name(FName,LName);
+// function multiplication(a,b,c){
+//     result=a+b+c;
+//     console.log(result);
+// }
+// let a=10;
+//     b=25;
+//     c=5;
+//     multiplication(a,b,c);
+
+//     function division(a,b,c,d,f){
+//         addition=a+b+c+d+f;
+//         avg=addition/2;
+//         console.log(avg);
+
+//     }
+//     let d=20;
+//         f=10;
+//         division(a,b,c,d,f);
+
+// function Add_Sub_Mul_Div_Avg(a,b,c,d,f)  {
+//     addition=a+b+c+d+f;
+//     multiplication=a*b*c*d*f;
+//     Div=addition/2;
+//     avg=addition/5;
+//     mod=addition%8;
+    // console.log(addition);
+    // console.log(multiplication);
+    // console.log(Div);
+    // console.log(avg);
+    // console.log(mod);
+//     console.log(addition,multiplication,Div,avg,mod);
+
+
+
+
+// }  
+// let a=10;b=20;c=30;d=40;f=50;
+
+//  Add_Sub_Mul_Div_Avg(a,b,c,d,f);
+
+
+
+// // Write a program to find all prime number from 1 to 100. 
+// function prime_Number( start,end){
+//     for(i=start;i<=end;i++){
+
+//         let n=i;
+//         is_prime=true;
+         
+//         for(j=2;j<i;j++){
+//             if(n%j==0)
+//             {
+//                 is_prime=false;
+//                 break;
+//             }
+    
+//         }
+//         if(is_prime){
+//             console.log(n);
+//         }
+    
+//     }
+//  }
+//  prime_Number(10,150);
+
+
+ 
+
+
+
+// Write a program to find all prime number from 1 to 100 using function.
+
+// function prime_Number(st,end){
+//     for(let i=st;i<=end;i++){
+
+    
+//         is_prime=true;
+        
+//         for(let k=2;k<i;k++){
+//             if(i%k==0){
+//                 is_prime=false;
+//                 break;
+//             }
+    
+//         }
+       
+//     if(is_prime=true)
+//     {
+//         console.log(i);
+//      } 
+        
+//      }
+// }
+// prime_Number(400,500);
+   
+// function print(Message, _Personal_details){
+//     console.log(Message,_Personal_details)
+// }
+// print("how are you, i am fine thank you", "He is not a good person");
+    
+// function MulDiv(a,b,c){
+//     mul=a*b;
+//     result=mul/c;
+//     console.log(result)
+// }
+// MulDiv(4,5,2);
+
+// function AddSubMulDivAvg(a,b,c,d,e){
+//     addition=a+b+c+d+e;
+//     Subtraction=addition-a;
+//     multiplication=a*b*c*d*e;
+//     division=addition/5;
+//     avarage=addition/5;
+//     console.log(addition,Subtraction,multiplication,division,avarage)
+
+   // }
+// // AddSubMulDivAvg(2,4,5,6,8); 
+
+ 
+//  x =30;
+//    y=10;
+//    console.log(y)
+// console.log(x-y)
+// console.log(x+y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x%y)
+// x+=20;//x=x+20
+// console.log(x)
+// x-=30;
+// console.log(x);//20
+// x*=10;
+// console.log(x);
+// x/=10;
+// console.log(x);//20
+// let ab=20;
+//     ac="20";
+// console.log(ab==ac)
+// console.log(ab===ac)
+// console.log(ab===ac)
+// x=0;
+// y=false;
+
+// console.log(x==y);
+// console.log(x===y);
+// console.log(typeof(y));
+// console.log(Number(y));
+// var driverXdistance=1;
+// var my_Location=true;
+// console.log(my_Location==driverXdistance)
+// console.log(my_Location===driverXdistance)
+// console.log(typeof(my_Location))
+
+
+// console.log(driverXdistance>5)
+// console.log(driverXdistance<5)
+// console.log(driverXdistance>=5)
+// console.log(driverXdistance<=5)
+
+const x=10;
+x=20;
+console.log(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
