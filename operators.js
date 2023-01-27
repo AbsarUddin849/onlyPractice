57caf2fcc03e8937b21a2f4aa96e92d4a64d2550
absar uddin <absaruddin849@gmail.com>
2023-01-27 19:32:13 +0000

    Logical operator class exercise

---
+++ operators.js
@@ -0,0 +1,69 @@
+var user_Name="Jone Doe";
+var password="absar1991";
+
+var bothCredsAreCorrect=user_Name==password;
+console.log(bothCredsAreCorrect); 
+
+var userName="absar1991";
+var pass_word="absar1991";
+var bothCredsAreCorrects=userName==pass_word;
+console.log(  bothCredsAreCorrects); 
+console.log(bothCredsAreCorrect&&bothCredsAreCorrects); 
+console.log(bothCredsAreCorrect||bothCredsAreCorrects); 
+
+var xx=8;
+var yy=8;
+console.log(!(8!=8))
+console.log(!(8==8))
+var x=20;
+var y=29;
+result=!x!=y;
+console.log(!(x!=y))
+console.log(result)
+
+var x=10;
+var y=20;
+var z=20;
+result=x!=y&&y!=z;
+result=x!=y&&y!=z;
+
+console.log(result)
+
+var x=10;
+var y=10;
+var z=25;
+console.log(x+y!=z&&z>=y+x);//true
+console.log(z==y&&z>x);
+console.log(z+x>z&&z>x);
+console.log(x+y>z&&y+z>z);
+console.log(x+y>z||y+z>z);
+console.log(x+y>z||y+z>z);
+console.log(x!=y!=z&&z>=y+x);//true
+
+user_Name="absar uddin";
+password="abdurRah1992";
+GmailId="absaruddin@1993";
+dateOfTest="30/01/27";
+isUserIdAndPasswordCorrect=user_Name=="absar uddin"&&password=="abdurRah1992";
+console.log(isUserIdAndPasswordCorrect);
+isGmailIdAndDateOfTest=GmailId=="absaruddin@1993"&&dateOfTest=="40/01/27";
+console.log(isGmailIdAndDateOfTest);
+finalResult=isGmailIdAndDateOfTest||isUserIdAndPasswordCorrect
+console.log(finalResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
